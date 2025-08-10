e671f814013340b9a9e759d5eeebcb29e3f92d86
oznur123456 <oznur.demirkaya@gmail.com>
2025-08-10 09:45:44 +0300

    Ders guncellemesi

--- Deneme1.py
+++ Deneme1.py
@@ -1,1 +1,16 @@
-.
+
+print("╔═════════════════════╗")
+print("║    OYUNLAR MENU     ║")
+print("║                     ║")
+print("║  1-Tetris           ║")
+print("║  2-Adam asmaca      ║")
+print("║  3-Yılan            ║")
+print("║  4-                 ║")
+print("║  5-                 ║")
+print("║  6-Ana menüye dön   ║")
+print("║                     ║")
+print("║    Seçimiz nedir?   ║")
+print("╚═════════════════════╝")
+ss = input()
+if ss == "1" : print("Tetris başlıyor..")
+if ss == "6":  print ("Ana menuye donuluyor.")
