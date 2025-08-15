0fd930b49908b969a15cbcf8be4a503241fe8b36
oznur123456 <oznur.demirkaya@gmail.com>
2025-08-15 23:11:46 +0300

    prooje1calisma

--- proje_oyunlar_menusu.py
+++ proje_oyunlar_menusu.py
@@ -1,22 +1,22 @@
 
 def oyun_menu():
 
-print("╔═════════════════════╗")
-print("║    OYUNLAR MENU     ║")
-print("║                     ║")
-print("║  1-Tetris           ║")
-print("║  2-Adam asmaca      ║")
-print("║  3-Yılan            ║")
-print("║  4-                 ║")
-print("║  5-                 ║")
-print("║  6-Ana menüye dön   ║")
-print("║                     ║")
-print("║    Seçimiz nedir?   ║")
-print("╚═════════════════════╝")
-ss = input()
-if ss == "1" : print("Tetris başlıyor..")
-if ss == "6":
-    import proje_ana_ekrani
+    print("╔═════════════════════╗")
+    print("║    OYUNLAR MENU     ║")
+    print("║                     ║")
+    print("║  1-Tetris           ║")
+    print("║  2-Adam asmaca      ║")
+    print("║  3-Yılan            ║")
+    print("║  4-                 ║")
+    print("║  5-                 ║")
+    print("║  6-Ana menüye dön   ║")
+    print("║                     ║")
+    print("║    Seçimiz nedir?   ║")
+    print("╚═════════════════════╝")
+    ss = input()
+    if ss == "1" : print("Tetris başlıyor..")
+    if ss == "6":
+        import proje_ana_ekrani
 
 oyun_menu()
     
