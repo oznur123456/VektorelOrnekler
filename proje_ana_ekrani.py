8859111030e753b5b7183107346dfdc63827137b
oznur123456 <oznur.demirkaya@gmail.com>
2025-08-15 19:54:14 +0300

    Yenidosyalar

---
+++ proje_ana_ekrani.py
@@ -0,0 +1,28 @@
+
+#print("╔"+"═"*20+"╗")
+print("╔═════════════════════╗")
+print("║    VEKTOREL APP     ║")
+print("║                     ║")
+print("║  1-Hesaplamalar     ║")
+print("║  2-Oyunlar          ║")
+print("║  3-Çizimler         ║")
+print("║  4-                 ║")
+print("║  5-                 ║")
+print("║  6-                 ║")
+print("║  7-                 ║")
+print("║  8-                 ║")
+print("║  9-                 ║")
+print("║ 10-                 ║")
+print("║                     ║")
+print("║    Seçimiz nedir?   ║")
+print("╚═════════════════════╝")
+secim = input() # input ile alınan bilgi string türündedir.
+print("Seçiminiz:",secim,"idi.")
+if secim == "1":
+    print("Hesaplamaları seçtiniz.")
+    import proje_oyunlar_menusu
+    
+if secim == "2": print("Oyunlar seçtiniz.")
+
+
+
