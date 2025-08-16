05f7bb3f6a7afeb7c69c31d5c4c72178f4e18edf
oznur123456 <oznur.demirkaya@gmail.com>
2025-08-16 09:34:19 +0300

    proje1calisma

--- proje_oyunlar_menusu.py
+++ proje_oyunlar_menusu.py
@@ -16,7 +16,16 @@ def oyun_menu():
     print("\033[0m") 
 
     ss = input()
-    if ss == "1" : print("Tetris başlıyor..")
+    if ss == "1" : print("Tetris basliyor...")
+        import
+    if ss == "2" : print("Adam asmaca basliyor...")
+        import
+    if ss == "3" : print("Yilan basliyor...")
+        import
+    if ss == "4" : print("Tetris basliyor...")
+        import
+    if ss == "5" : print("Tetris basliyor..")
+        import
     if ss == "6":
         import proje_ana_ekrani
 
