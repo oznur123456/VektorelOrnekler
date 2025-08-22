50c15819fb987f3d463dbb4c7d0730517818c760
oznur123456 <oznur.demirkaya@gmail.com>
2025-08-22 19:29:24 +0300

    ornek

---
+++ H3_2.py
@@ -0,0 +1,25 @@
+meyveler = [] # Boş bir liste/dizi oluşturur
+
+print(meyveler) # Boş listeyi ekrana yazdırır
+
+meyveler.append("Elma") # Listeye "Elma" öğesini ekler
+print(meyveler) 
+# meyveler.append("Nar",'Muz')
+meyveler += ["Nar", "Muz"] # Listeye "Nar" ve "Muz" öğelerini ekler 
+print(meyveler) 
+
+
+
+
+meyveler = [] # Boş bir liste/dizi oluşturur
+
+print(meyveler) # Boş listeyi ekrana yazdırır
+
+meyveler.append("Elma") # Listeye "Elma" öğesini ekler
+print(meyveler) 
+# meyveler.append("Nar",'Muz')
+meyveler += ["Nar", "Muz"] # Listeye "Nar" ve "Muz" öğelerini ekler 
+print(meyveler) 
+
+meyveler.pop()
+print(meyveler) 
